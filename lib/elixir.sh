d5d9b2e7895aa2399a097920a0e9bb15527bfa48
Tyler Flint <tylerflint@gmail.com>
2016-10-08 12:48:45 -0600

    Start to refactor the build pattern to fit with v1 pattern

--- lib/elixir.sh
+++ lib/elixir.sh
@@ -1,19 +1,39 @@
 # -*- mode: bash; tab-width: 2; -*-
 # vim: ts=2 sw=2 ft=bash noet
 
-runtime() {
-	echo $(nos_validate "$(nos_payload 'config_runtime')" "string" "elixir")
+# Copy the compiled jars into the app directory to run live
+publish_release() {
+	nos_print_bullet "Moving build into live app directory..."
+	rsync -a $(nos_code_dir)/ $(nos_app_dir)
 }
 
-install_runtime() {
-  pkgs=($(runtime))
+# Determine the elixir runtime to install. This will first check
+# within the boxfile.yml, then will rely on default_runtime to
+# provide a sensible default
+runtime() {
+  echo $(nos_validate \
+    "$(nos_payload "config_runtime")" \
+    "string" "$(default_runtime)")
+}
+
+# Provide a default elixir version.
+default_runtime() {
+  echo "elixir-1.3"
+}
+
+# Install the elixir and erlang runtime along with any dependencies.
+install_runtime_packages() {
+  pkgs=("$(runtime)")
+  
+  # add any client dependencies
+  pkgs+=("$(query_dependencies)")
 
   nos_install ${pkgs[@]}
 }
 
 # Uninstall build dependencies
 uninstall_build_packages() {
-  # currently ruby doesn't install any build-only deps... I think
+  # currently elixir doesn't install any build-only deps... I think
   pkgs=()
 
   # if pkgs isn't empty, let's uninstall what we don't need
@@ -22,11 +42,35 @@ uninstall_build_packages() {
   fi
 }
 
-mix_dir() {
-  [[ ! -f $(nos_code_dir)/.mix ]] && nos_run_process "make mix cache dir" "mkdir -p $(nos_code_dir)/.mix"
-  [[ ! -s ${HOME}/.mix ]] && nos_run_process "link mix cache dir" "ln -s $(nos_code_dir)/.mix ${HOME}/.mix"
-  [[ ! -f $(nos_code_dir)/.hex ]] && nos_run_process "make hex cache dir" "mkdir -p $(nos_code_dir)/.hex"
-  [[ ! -s ${HOME}/.hex ]] && nos_run_process "link hex cache dir" "ln -s $(nos_code_dir)/.hex ${HOME}/.hex"
+# compiles a list of dependencies that will need to be installed
+query_dependencies() {
+  deps=()
+
+  # mysql
+  # memcache
+  # postgres
+  # redis
+  
+  echo "${deps[@]}"
+}
+
+# create cache directories and link mix to them
+setup_mix() {
+	if [[ ! -f $(nos_code_dir)/.mix ]]; then
+		mkdir -p $(nos_code_dir)/.mix
+	fi
+	
+	if [[ ! -s ${HOME}/.mix ]]; then
+		ln -s $(nos_code_dir)/.mix ${HOME}/.mix
+	fi
+	
+	if [[ ! -f $(nos_code_dir)/.hex ]]; then
+		mkdir -p $(nos_code_dir)/.hex
+	fi
+	
+	if [[ ! -s ${HOME}/.hex ]]; then
+		ln -s $(nos_code_dir)/.hex ${HOME}/.hex
+	fi
 }
 
 create_profile_links() {
@@ -63,10 +107,4 @@ compile_deps() {
 
 compile() {
 	(cd $(nos_code_dir); nos_run_process "mix compile" "mix compile --force")
-}
-
-# Copy the compiled jars into the app directory to run live
-publish_release() {
-	nos_print_bullet "Moving build into live code directory..."
-	rsync -a $(nos_code_dir)/ $(nos_app_dir)
 }
